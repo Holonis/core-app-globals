9979e773d6714e6c4713e6496617eb9abb4f33e1
Jason Kell <jkell@holonis.com>
2019-12-06 13:37:40 -0800

    feat: add kafkaProperties support

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 class Globals {
   constructor() {
     let globalConfig;
+    let globalKafkaProperties;
     let globalLog;
 
     this.getConfig = () => globalConfig;
+
+    this.getKafkaProperties = () => globalKafkaProperties;
 
     this.getLog = () => globalLog;
 
     this.setConfig = (config) => {
       globalConfig = config;
+    };
+
+    this.setKafkaProperties = (kafkaProperties) => {
+      globalKafkaProperties = kafkaProperties;
     };
 
     this.setLog = (log) => {
